1c9d966b3821df4b32711e2f6a94c6637f28aae4
Sharanya Haridas <sharanya0211@gmail.com>
2014-03-07 17:34:18 -0800

    with new variable and function names

--- scripts.js
+++ scripts.js
@@ -2,29 +2,32 @@
  * @author Sharanya Haridas
  */
 
-/*Project Steps:
- * 1. Set up document ready
- * 2. Load Google Viz library,Google charts package
- * 3.Load data
- * 4.Render chart
- */
 
-/*page loaded function, responds to document ready*/
+	
 
-function dataLoaded(UNEMPDATA) {
-	console.log(UNEMPDATA);
+/*As part of the assignment, I changed varible and function names as follows:
+ *myChart to mChart,
+ * gDataTable to gTable
+ * UNEMPDATA to CIVUNEMP
+ * ChartOptions to ChartOpt
+ * options to mOptions
+ * googleVizloaded to gVizloaded
+ * pageDone to mDone*/
 
-	var gDataTable = new google.visualization.DataTable();
+function dataLoaded(CIVUNEMP) {
+	console.log(CIVUNEMP);
+
+	var gTable = new google.visualization.DataTable();
 
 	//when i add columns, the first parameter is the data type in that column
 	//the second parameter is the name of that column
 
-	gDataTable.addColumn('string', UNEMPDATA.columns[0]);
-	gDataTable.addColumn('number', UNEMPDATA.columns[1]);
+	gTable.addColumn('string', CIVUNEMP.columns[0]);
+	gTable.addColumn('number', CIVUNEMP.columns[1]);
 
-	gDataTable.addRows(UNEMPDATA.rows);
+	gTable.addRows(CIVUNEMP.rows);
 
-	var ChartOptions = {
+	var ChartOpt = {
 
 		title : "Unemployment since 1948"
 	};
@@ -32,7 +35,7 @@ function dataLoaded(UNEMPDATA) {
 	//tell it to create a line chart
 
 	/*
-	 var myObsArray =unempData.observations;
+	 var myObsArray =CivunempData.observations;
 	 console.log(myObsArray);
 	 var myDataList = [];
 
@@ -58,37 +61,37 @@ function dataLoaded(UNEMPDATA) {
 	 var myDataTable = google.visualization.arrayToDataTable(myDataList);
 	 */
 
-	var options = {
-		title : "Unemployment Data Over Time"
+	var mOptions = {
+		title : "Unemployment Data: 2000 to present"
 	};
 
 	//document.getelementbyid is the equivalent of jquery's $ sign with div name
-	var myChart = new google.visualization.LineChart(document.getElementById("myChartDiv"));
+	var mChart = new google.visualization.LineChart(document.getElementById("mChartDiv"));
 
-	myChart.draw(gDataTable, options);
+	mChart.draw(gTable, mOptions);
 
 }
 
-function googleVizloaded() {
+function gVizloaded() {
 
-	console.log("google viz loaded!");
+	console.log("google visualization is loaded!");
 
 	//get function for loading data
 
 	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1Kg4n_bZlh1j5xK_KG8KxJWFjRz4Ia4r-HS8-iCUZ+WHERE+DATE%3E%20'2000-12-01'&key=AIzaSyDo19_AYa5DRvvwPl9RmRIlsSxnmlbklqg", dataLoaded, "json");
 }
 
-function pageDone() {
+function mDone() {
 	console.log("page done!");
 
 	//load google charting package
 
 	google.load("visualization", "1", {
 		packages : ["corechart"],
-		"callback" : googleVizloaded
+		"callback" : gVizloaded
 	});
 }
 
 /*document ready function*/
 
-$(document).ready(pageDone);
+$(document).ready(mDone);
